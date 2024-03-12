a5f3f4b303929e6f8f94b89b1a8bb191336f85cc
Salva <s.garcia.jimenez@iescristobaldemonroy.es>
2024-03-12 20:18:17 +0100

    Salvador Garcia Jimenez documentacion

---
+++ RDE08/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Habitacion"},{"p":"<Unnamed>","l":"Hotel"},{"p":"<Unnamed>","l":"HotelException"},{"p":"<Unnamed>","l":"Principal"}];updateSearchResults();
